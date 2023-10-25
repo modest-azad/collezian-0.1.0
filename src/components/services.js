6159be402c20672801c12fbca0a1db40d7beaa2b
modest_azad <collezian@gmail.com>
2023-10-25 14:46:51 +0530

    spacing need to fixex

--- src/components/services.js
+++ src/components/services.js
@@ -2,19 +2,53 @@
 function services() {
   return (
     <>
-      <div className="services container">
-        <div className="feature grid grid-rows-2 gap-10 md:grid-cols-5">
+      <div className="service p-[5%] md:p[40%] ">
+        {/* feature - 1  */}
+        <div className="feature grid max-sm:grid-rows-2 md:grid-cols-5 items-center">
           <div className="feature-image md:col-span-2 ">
-            <img
-              
-              src="/images/man-reading-book.svg"
-              alt="readingBook"
-            />
+            <img src="/images/man-reading-book.svg" alt="readingBook" />
           </div>
 
-          <div className="feature-content max-sm:text-center  md:col-span-3 p-8">
+          <div className="feature-content max-sm:text-center md:p-8 md:col-span-3 ">
             <h2 className="font-bold  text-2xl">Grow with AI</h2>
-            <p className="py-4">
+            <p className="py-4 text-gray-600">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+              est ea veritatis sed reiciendis nostrum quam vitae velit
+              recusandae? Culpa incidunt aut error a earum iusto commodi, modi
+              architecto? Quidem minus, quos corrupti sint est exercitationem.
+              Dolor, recusandae ipsum consequatur in, expedita ipsa dolorum
+              exercitationem fugit, voluptas tempore doloribus iste!
+            </p>
+          </div>
+        </div>
+
+        {/* feature - 2 */}
+        <div className="feature grid max-sm:grid-rows-2 md:grid-cols-5 items-center">
+          <div className="feature-image md:col-span-2 ">
+            <img src="/images/education.png" alt="Study_Materials" />
+          </div>
+          <div className="feature-content max-sm:text-center text-right md:p-8 md:col-span-3 ">
+            <h2 className="font-bold text-2xl">Free Engineering Materials</h2>
+            <p className="py-4 text-gray-600">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+              est ea veritatis sed reiciendis nostrum quam vitae velit
+              recusandae? Culpa incidunt aut error a earum iusto commodi, modi
+              architecto? Quidem minus, quos corrupti sint est exercitationem.
+              Dolor, recusandae ipsum consequatur in, expedita ipsa dolorum
+              exercitationem fugit, voluptas tempore doloribus iste!
+            </p>
+          </div>
+        </div>
+
+        {/* feature - 3  */}
+        <div className="feature grid max-sm:grid-rows-2 md:grid-cols-5 items-center">
+          <div className="feature-image md:col-span-2 ">
+            <img src="/images/happy-graduation.svg" alt="career" />
+          </div>
+
+          <div className="feature-content max-sm:text-center md:p-8 md:col-span-3 ">
+            <h2 className="font-bold  text-2xl">Job Updates</h2>
+            <p className="py-4 text-gray-600">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
               est ea veritatis sed reiciendis nostrum quam vitae velit
               recusandae? Culpa incidunt aut error a earum iusto commodi, modi
