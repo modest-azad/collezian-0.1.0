377b78642a3dab0f5e06b5b692a27e8ee195b2e3
modest_azad <collezian@gmail.com>
2023-10-27 19:48:24 +0530

    without react component file removed

--- src/pages/branch/Branch_Notes.js
+++ src/pages/branch/Branch_Notes.js
@@ -50,16 +50,17 @@ export default function Branch_Notes({ name }) {
 
         {/* contents  */}
         <div className=" grid grid-cols-1 md:grid-cols-3 gap-10 md:px-[10%] px-[5%] py-[5%]">
-        <div  className="card flex bg-blue-100 text-center shadow rounded-xl">
+          {Branch_Notes_api.map((currentData) => {          
+                <div key={currentData.key} className="card flex bg-blue-100 text-center shadow rounded-xl">
                   <div className="content p-4">
                     <h2 className="font-semibold text-lg">
-                       Subject Name
+                      {currentData.subject_name}
                     </h2>
                     <h3 className="text-sm text-gray-700">
-                      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus non expedita veritatis, voluptatum officia autem?
+                      {currentData.description}
                     </h3>
                     <p className="py-2 text-sm  text-gray-700">
-                      Uploads: 1 day ago
+                      Uploads: {currentData.uploads}
                     </p>
                     <div className=" text-sm user-feedback text-gray-700 flex justify-center gap-4 cursor-pointer">
                       <div className="view-count pr-2"> 0 view</div>
@@ -68,6 +69,7 @@ export default function Branch_Notes({ name }) {
                     </div>
                   </div>
                 </div>
+          })}
         </div>
       </div>
     </>
