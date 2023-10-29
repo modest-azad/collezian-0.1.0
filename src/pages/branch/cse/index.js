9389bc4c6bc3d6390bdfd7aec6752563eb4b8e8d
modest_azad <collezian@gmail.com>
2023-10-29 10:38:15 +0530

    link removed

--- src/pages/branch/cse/index.js
+++ src/pages/branch/cse/index.js
@@ -1,10 +1,9 @@
 import Branch_template from "../Branch_template";
-import Assignment from "./assignment";
 
 export default function cse(){
     return(
         <>
-            <Branch_template name="Computer Science and Engineering" notes={"./cse/notes"} />
+            <Branch_template name="Computer Science and Engineering" />
         </>
     )
 }
