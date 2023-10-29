ff1daadabc9e5b5e45959b41440f7579ea25378f
modest_azad <collezian@gmail.com>
2023-10-29 12:08:17 +0530

    Link added in notes using props

--- src/pages/branch/ee/index.js
+++ src/pages/branch/ee/index.js
@@ -1,8 +1,9 @@
 import Branch_index from "../Branch_index"
 export default function ee(){
+  
     return(
         <>
-            <Branch_index name="Electrical Engineering"/>
+            <Branch_index name="Electrical Engineering" link="./ee/notes" />
         </>
     )
 }
