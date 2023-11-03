35fcd1261965ffa118747477fbbe48c149a55c6e
modest_azad <collezian@gmail.com>
2023-11-03 10:19:07 +0530

    4 col

--- src/components/Important_portal.js
+++ src/components/Important_portal.js
@@ -6,7 +6,7 @@ function Portals(){
         <>
                 <h1 id="portal-links" className="text-center text-2xl md:text-3xl font-semibold my-4">Important Portal</h1>
             <div className="important-portal rounded-3xl md:mx-[10%]">
-                <div className="items text-center text-xs grid grid-cols-4 justify-center lg:grid-cols-6  md:grid-cols-4 gap-5 py-9 px-[5%]">
+                <div className="items text-center text-xs grid grid-cols-3 justify-center lg:grid-cols-6  gap-5 py-9 px-[5%]">
                 <a href="null" className="item transition-all duration-300 ease-in-out hover:translate-y-[-1em] ">
                         <img className="w-full p-4 rounded-full" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKUd5HnBqh0VBnFzcWlyz_32Az6uxtP1ahatKFttg&s" alt="image" />
                         <h2>NPTEL</h2>
