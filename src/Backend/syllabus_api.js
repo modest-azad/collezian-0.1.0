dc328bb5f576bc5ac2ab4158d992e3bbf71084d4
modest_azad <collezian@gmail.com>
2023-11-07 23:37:38 +0530

    nothing

---
+++ src/Backend/syllabus_api.js
@@ -0,0 +1,69 @@
+export  const cse = [
+    {
+        board:"beu",
+        branch:"cse",
+        sem:"1st Semester",
+        link:"#"
+    },
+    {   board:"beu",
+        branch:"cse",
+        sem:"2nd Semester",
+        link:"#"
+    },
+]
+
+
+export const ece = [
+    {
+        board:"beu",
+        branch:"ece",
+        sem:"1st Semester",
+        link:"#"
+    },
+]
+
+
+export const eee = [
+    {
+        board:"beu",
+        branch:"eee",
+        sem:"1st Semester",
+        link:"#"
+    },
+]
+export const ee = [
+    {
+        board:"beu",
+        branch:"ee",
+        sem:"1st Semester",
+        link:"#"
+    },
+    {
+        board:"beu",
+        branch:"ee",
+        sem:"2nd Semester",
+        link:"#"
+    },
+    {
+        board:"beu",
+        branch:"ee",
+        sem:"3rd Semester",
+        link:"#"
+    },
+]
+export const civil = [
+    {
+        board:"beu",
+        branch:"civil",
+        sem:"1st Semester",
+        link:"#"
+    },
+]
+export const me = [
+    {
+        board:"beu",
+        branch:"me",
+        sem:"1st Semester",
+        link:"#"
+    },
+]
--- src/pages/branch/ee/syllabus.js
+++ src/pages/branch/ee/syllabus.js
@@ -1,11 +1,12 @@
 import Branch_syllabus from "../Branch_syllabus";
-
+import {ee} from"../../../Backend/syllabus_api";
 
 export default function syllabus(){
     const heading = "Electrical Engineering Syllabus"
+
     return (
         <>
-            <Branch_syllabus heading={heading}/>
+            <Branch_syllabus heading={heading} data={ee}/>
         </>
     )
 }
